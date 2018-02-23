daf11f7578b4b4592ab25d09b61bf13785d42d84
nntrongnghia <nntrongnghiadt@gmail.com>
2018-02-23 12:47:58 +0100

    ajoute chatbot

--- chatbot.py
+++ chatbot.py
@@ -1,12 +1,13 @@
 from fbchat import Client
 from fbchat.models import *
-import time
 
-#ceci est juste une exemple de chatbot, il ne faut pas 'SPAM' facebook par login et logout trop vite.
+client = Client('hakimtsouria@gmail.com', 'kimodu06')
 def send_message(message):
-    #remplacer <email> et <password> par un compte de facebook
-    client = Client('<email>', '<password>')
+    if not client.isLoggedIn():
+        client.login('hakimtsouria@gmail.com', 'kimodu06')
     client.send(Message(text=message), thread_id=client.uid, thread_type=ThreadType.USER)
-    time.sleep(1)
+    return None
+
+def logout():
     client.logout()
     return None
